655f905cc60bc36001f122e04ce6c5355cee6c58
Brad Burch <bradburch77@gmail.com>
2016-11-02 22:40:49 -0400

    Fixing Deployment issues

---
+++ config/config.js
@@ -0,0 +1,9 @@
+module.exports =
+{
+  'secret': 'ilovescotchscotchyscotchscotch',
+  //'database': 'postgres://postgres:s@localhost:5432/JoggersLoggersDB',
+  'CLIENT_ID': '227WYN',
+  'CLIENT_SECRET': 'df1a88f6d18e0791265edbbe02781ee1',
+  'CALLBACK_URL': 'https://joggersandloggers.herokuapp.com/api/v1/fitbit/callback',
+  'database': 'process.env.DATABASE_URL'
+}
