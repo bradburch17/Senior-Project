5cd45afb970a05fb0a502c3e5091184a10b36a97
Brad Burch <bradburch77@gmail.com>
2016-11-30 21:30:55 -0500

    Needed to change from localhost

--- routes/index.js
+++ routes/index.js
@@ -263,8 +263,8 @@ function sendEmail(token, email) {
         subject: 'Joggers and Loggers Password Reset',
         text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
             'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-            // 'https://joggersandloggers.herokuapp.com/#/passwordchange/' + token + '\n\n' +
-            'http://localhost:3000/#/passwordchange/' + token + '\n\n' +
+            'https://joggersandloggers.herokuapp.com/#/passwordchange/' + token + '\n\n' +
+            // 'http://localhost:3000/#/passwordchange/' + token + '\n\n' +
             'If you did not request this, please ignore this email and your password will remain unchanged.\n'
     };
 
